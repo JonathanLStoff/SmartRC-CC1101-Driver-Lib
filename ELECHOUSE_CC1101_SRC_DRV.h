74237667f2a081e37fc54cc3522a482417fe7728
Nerfbilly64 <jonstoffex@comcast.net>
2026-01-27 20:29:18 -0500

    SerialUSB Serial

--- ELECHOUSE_CC1101_SRC_DRV.h
+++ ELECHOUSE_CC1101_SRC_DRV.h
@@ -128,7 +128,7 @@ private:
   void Split_MDMCFG2(void);
   void Split_MDMCFG4(void);
 public:
-  void Init(void);
+  void Init(SerialUSB Serial);
   byte SpiReadStatus(byte addr);
   void setSpiPin(byte sck, byte miso, byte mosi, byte ss);
   void addSpiPin(byte sck, byte miso, byte mosi, byte ss, byte modul);
